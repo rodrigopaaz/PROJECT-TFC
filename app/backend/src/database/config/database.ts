a3b52dc088f8159e26290c1e60022da6c815594b
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-05-13 15:07:42 -0300

    chore: update versel json 5

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -8,7 +8,7 @@ const config: Options = {
   password: process.env.MYSQLPASSWORD || '123456',
   database: process.env.MYSQLDATABASE || 'railway',
   host: process.env.MYSQLHOST || 'localhost',
-  port: Number(process.env.MYSQLPORT),
+  port: Number(process.env.MYSQLPORT), // caso rode localmente substituir por 3002
   dialect: 'mysql',
   dialectModule: mysql2,
   dialectOptions: {
